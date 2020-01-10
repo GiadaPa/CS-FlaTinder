f3723837f05220ad574ac9898b87115066abc0f5
Giada <40485958+GiadaPa@users.noreply.github.com>
2020-01-10 21:16:49 +0100

    Update style.js

--- style.js
+++ style.js
@@ -10,6 +10,13 @@ const styles = StyleSheet.create({
 		backgroundColor: '#f5fcff',
 		padding: 11
 	},
+	MainContainerProfile: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		backgroundColor: '#f5fcff',
+		padding: 11
+	},
 	titleText: {
 		fontWeight: 'bold',
 		fontSize: 30,
@@ -33,17 +40,61 @@ const styles = StyleSheet.create({
 		borderWidth:1,
 		height:40,
 		width: 300,
-		margin: 20,
+		marginTop: 5,
+		marginRight:20,
+		marginBottom: 20,
 	},
-	text: {
+	textQuestions: {
+		fontWeight: 'bold',
 		fontSize: 15,
 		color: 'black',
+		marginTop: 10
 	},
 	underlinedText: {
 		fontSize: 15,
 		color: 'red',
 		textDecorationLine: 'underline',
 	},
+	radioTitle: {
+		fontWeight: 'bold',
+	},
+	TextMyname: {
+		fontWeight: 'bold',
+		fontSize: 30,
+		margin: 10,
+	},
+	myPicStyle:{
+		maxHeight:200, 
+		maxWidth: 350,
+	},
+	constrPersonal: {
+		justifyContent: 'flex-start',
+		alignItems: 'baseline',
+		backgroundColor: '#f5fcff',
+		flexDirection: 'row'
+	},
+	infoContainer:{
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'lightgrey',
+		padding: 20
+	},
+	infoProfile:{
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		backgroundColor: '#f5fcff',
+		padding: 11
+	},
+	infoTextTitle:{
+		fontWeight: 'bold',
+		marginTop: 20,
+		fontSize: 20,
+	},
+	infoText:{
+		fontSize: 20,
+	},
 }); 
 
-export default styles
+export default styles;
