62d03267cad41461a5b0d698755a9813c1f297d6
Giada <40485958+GiadaPa@users.noreply.github.com>
2020-01-13 11:42:07 +0100

    Update Questionnaire_Screen.js

--- screens/Questionnaire_Screen.js
+++ screens/Questionnaire_Screen.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {View, ScrollView, Text, TextInput, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {View, ScrollView, Text, TextInput, TouchableOpacity, Slider} from 'react-native';
 import RadioGroup, {Radio} from "react-native-radio-input";
 import {Subscribe} from 'unstated'
 
@@ -14,73 +14,153 @@ const Questionnaire_Screen = props => {
 			(
 				<View style={styles.MainContainer}>
 					<ScrollView>
-						<Text style={styles.text}>Maximum budget for rent:</Text>
-						<TextInput
-							style = {styles.textInput}
-							onChangeText = {(budget) => container.setTempConstraints(budget, "budget")}
-							keyboardType={'numeric'}
-						>
-						</TextInput>
-						<Text style={styles.text}>Maximum number of flatmates:</Text>
-						<TextInput
-							style = {styles.textInput}
-							onChangeText = {(flatmates) => container.setTempConstraints(flatmates, "flatmates")}
-							keyboardType={'numeric'}
-						>
-						</TextInput>
-						<Text style={styles.text}>Minimum room size:</Text>
-						<TextInput
-							style = {styles.textInput}
-							onChangeText = {(size) => container.setTempConstraints(size, "size")}
-							keyboardType={'numeric'}
-						>
-						</TextInput>
-						<Text>Do you want roommates?</Text>
-						<RadioGroup getChecked = {(value) => container.setTempConstraints(value, "roommates")}>
-							<Radio iconName = {"lens"} label = {"Yes"} value = {"true"}/>
-							<Radio iconName = {"lens"} label = {"No"} value = {"false"}/>
-						</RadioGroup>
-						<Text>What kind of personality are you?</Text>
-						<RadioGroup getChecked = {(value) => container.setTempConstraints(value, "personality")}>
-							<Radio iconName = {"lens"} label = {"Introverted"} value = {"introvert"}/>
-							<Radio iconName = {"lens"} label = {"Ambiverted"} value = {"ambivert"}/>
-							<Radio iconName = {"lens"} label = {"Extroverted"} value = {"extrovert"}/>
-						</RadioGroup>
-						<Text>Are you talkative?</Text>
-						<RadioGroup getChecked = {(value) => container.setTempConstraints(value, "talkative")}>
-							<Radio iconName = {"lens"} label = {"Shh"} value = {"little"}/>
-							<Radio iconName = {"lens"} label = {"Bla"} value = {"medium"}/>
-							<Radio iconName = {"lens"} label = {"BlaBlaBla"} value = {"high"}/>
-						</RadioGroup>
-						<Text style={styles.text}>How much are these things important to you? (1: not at all, 10: a lot)</Text>
-						<Text style={styles.text}>Cleaness</Text>
-						<TextInput
-							style = {styles.textInput}
-							onChangeText = {(clean) => container.setTempConstraints(clean, "clean")}
-							keyboardType={'numeric'}
-						>
-						</TextInput>
-						<Text style={styles.text}>Healthy food</Text>
-						<TextInput
-							style = {styles.textInput}
-							onChangeText = {(food) => container.setTempConstraints(food, "food")}
-							keyboardType={'numeric'}
-						>
-						</TextInput>
-						<Text style={styles.text}>Studying or Working</Text>
-						<TextInput
-							style = {styles.textInput}
-							onChangeText = {(study) => container.setTempConstraints(study, "study")}
-							keyboardType={'numeric'}
-						>
-						</TextInput>
-						<Text style={styles.text}>Partying and going out</Text>
-						<TextInput
-							style = {styles.textInput}
-							onChangeText = {(party) => container.setTempConstraints(party, "party")}
-							keyboardType={'numeric'}
-						>
-						</TextInput>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Maximum budget for rent (€):</Text>
+							<TextInput
+								style = {styles.textInput}
+								onChangeText = {(budget) => container.setTempConstraints(budget, "budget")}
+								keyboardType={'numeric'}
+							>
+							</TextInput>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Maximum number of flatmates:</Text>
+							<TextInput
+								style = {styles.textInput}
+								onChangeText = {(flatmates) => container.setTempConstraints(flatmates, "flatmates")}
+								keyboardType={'numeric'}
+							>
+							</TextInput>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Minimum room size (m^2):</Text>
+							<TextInput
+								style = {styles.textInput}
+								onChangeText = {(size) => container.setTempConstraints(size, "size")}
+								keyboardType={'numeric'}
+							>
+							</TextInput>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Do you want roommates?</Text>
+							<RadioGroup row = {true} getChecked = {(value) => container.setTempConstraints(value, "roommates")}>
+								<Radio iconName = {"lens"} label = {"Yes"} value = {"true"}/>
+								<Radio iconName = {"lens"} label = {"No"} value = {"false"}/>
+							</RadioGroup>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>What kind of personality are you?</Text>
+							<RadioGroup getChecked = {(value) => container.setTempConstraints(value, "personality")}>
+								<Radio iconName = {"lens"} label = {"Introverted"} value = {"introvert"}/>
+								<Radio iconName = {"lens"} label = {"Ambiverted"} value = {"ambivert"}/>
+								<Radio iconName = {"lens"} label = {"Extroverted"} value = {"extrovert"}/>
+							</RadioGroup>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Are you talkative?</Text>
+							<RadioGroup getChecked = {(value) => container.setTempConstraints(value, "talkative")}>
+								<Radio iconName = {"lens"} label = {"Shh"} value = {"little"}/>
+								<Radio iconName = {"lens"} label = {"Bla"} value = {"medium"}/>
+								<Radio iconName = {"lens"} label = {"BlaBlaBla"} value = {"high"}/>
+							</RadioGroup>
+						</View>
+							<Text style={styles.text}>How much are these things important to you?</Text>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Cleaness</Text>
+							<Slider
+								step = {1}
+								maximumValue = {9}
+								onValueChange = {(clean) => container.setTempConstraints(clean, "clean")}
+							/>
+							<View style = {styles.row}>
+								<Text>0</Text>
+								<Text>1</Text>
+								<Text>2</Text>
+								<Text>3</Text>
+								<Text>4</Text>
+								<Text>5</Text>
+								<Text>6</Text>
+								<Text>7</Text>
+								<Text>8</Text>
+								<Text>9</Text>
+							</View>
+							<View style = {styles.row}>
+								<Text>not at all</Text>
+								<Text>a lot</Text>
+							</View>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Healthy food</Text>
+							<Slider
+								step = {1}
+								maximumValue = {9}
+								onValueChange = {(food) => container.setTempConstraints(food, "food")}
+							/>
+							<View style = {styles.row}>
+								<Text>0</Text>
+								<Text>1</Text>
+								<Text>2</Text>
+								<Text>3</Text>
+								<Text>4</Text>
+								<Text>5</Text>
+								<Text>6</Text>
+								<Text>7</Text>
+								<Text>8</Text>
+								<Text>9</Text>
+							</View>
+							<View style = {styles.row}>
+								<Text>not at all</Text>
+								<Text>a lot</Text>
+							</View>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Studying or Working</Text>
+							<Slider
+								step = {1}
+								maximumValue = {9}
+								onValueChange = {(study) => container.setTempConstraints(study, "study")}
+							/>
+							<View style = {styles.row}>
+								<Text>0</Text>
+								<Text>1</Text>
+								<Text>2</Text>
+								<Text>3</Text>
+								<Text>4</Text>
+								<Text>5</Text>
+								<Text>6</Text>
+								<Text>7</Text>
+								<Text>8</Text>
+								<Text>9</Text>
+							</View>
+							<View style = {styles.row}>
+								<Text>not at all</Text>
+								<Text>a lot</Text>
+							</View>
+						</View>
+						<View style = {styles.questionnaireView}>
+							<Text style={styles.text}>Partying and going out</Text>
+							<Slider
+								step = {1}
+								maximumValue = {9}
+								onValueChange = {(party) => container.setTempConstraints(party, "party")}
+							/>
+							<View style = {styles.row}>
+								<Text>0</Text>
+								<Text>1</Text>
+								<Text>2</Text>
+								<Text>3</Text>
+								<Text>4</Text>
+								<Text>5</Text>
+								<Text>6</Text>
+								<Text>7</Text>
+								<Text>8</Text>
+								<Text>9</Text>
+							</View>
+							<View style = {styles.row}>
+								<Text>not at all</Text>
+								<Text>a lot</Text>
+							</View>
+						</View>
 					</ScrollView>
 					<TouchableOpacity
 						style={styles.button}
