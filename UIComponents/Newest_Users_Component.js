21b49afcf555af39118badc01502f77b19e8e290
Giada <40485958+GiadaPa@users.noreply.github.com>
2020-01-15 20:52:50 +0100

    Update Newest_Users_Component.js

--- UIComponents/Newest_Users_Component.js
+++ UIComponents/Newest_Users_Component.js
@@ -2,42 +2,53 @@
 import React from 'react';
 import { TouchableHighlight,ScrollView, View, Text, Image, Button } from 'react-native';
 
+import { Divider } from 'react-native-elements';
 import styles from '../style'
 
 
 const Newest_Users_Component = props => (
-    <ScrollView
-	horizontal={true}
-	>
-		{
-			props.users.map((user, index) => ( //invert array
-				<TouchableHighlight
-					key = {user.username}
-					onPress= {() => props.selectUser(index)}
-				>
-					<View style = {styles.MainContainerProfile}>
-						{user.image == ''?
-							(user.info.gender == 'female'?
-								<Image
-									source={require('../assets/femalepic.jpg')}
-									style = {{width:100, height:100}}
-								/>:
-								<Image
-									source={require('../assets/malepic.jpg')}
-									style = {{width:100, height:100}}
-								/>
-							):
-							(<Image
-								source={{uri: user.image}}
-								style = {{width:100, height:100}}
-							/>)
-						}
-						<Text>{user.username}</Text>
-					</View>
-				</TouchableHighlight>
-			))
-		}
-    </ScrollView>
+	<View>
+	<View>
+	<Text style={styles.joiningUsers}>Users that joined FlaTinder recently</Text>
+		<ScrollView
+			horizontal={true}
+			bounces={false}
+			contentContainerStyle={{alignItems:'flex-start'}}
+			>
+				{
+					props.users.reverse().map((user, index) => ( //invert array
+						<TouchableHighlight
+							key = {user.username}
+							onPress= {() => props.selectUser(index)}
+						>
+							<View style = {styles.Scroll}>
+								{user.image == ''?
+									(user.info.gender == 'female'?
+										<Image
+											source={require('../assets/femalepic.jpg')}
+											style = {{width:100, height:100}}
+										/>:
+										<Image
+											source={require('../assets/malepic.jpg')}
+											style = {{width:100, height:100}}
+										/>
+									):
+									(<Image
+										source={{uri: user.image}}
+										style = {{width:100, height:100}}
+									/>)
+								}
+								<Text style={styles.infoText}>{user.username}</Text>
+								<Text style={styles.infoText}>Liked: </Text>
+								</View>
+							</TouchableHighlight>
+					))
+				}
+		</ScrollView>
+		</View>
+		<Divider style={{ height: 2, backgroundColor: 'black' }} />
+	</View>
 )
 
+//<Text>{props.userLikes}</Text>
 export default Newest_Users_Component;
